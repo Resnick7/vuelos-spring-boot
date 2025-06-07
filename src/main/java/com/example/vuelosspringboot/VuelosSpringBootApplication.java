59d906207d31159c7e8d874f642f47afd966ba69
antoc <antocapadona7@gmail.com>
2025-06-07 13:16:36 -0300

    First commit

---
+++ src/main/java/com/example/vuelosspringboot/VuelosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.vuelosspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuelosSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VuelosSpringBootApplication.class, args);
+    }
+
+}
